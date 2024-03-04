fb8ba1bd8a240b513b8342c9c991a0760afc9bb0
Ivan <ivanrakov540@gmail.com>
2024-03-04 10:04:54 +0500

    add mock data

--- src/view/edit-event-view.js
+++ src/view/edit-event-view.js
@@ -1,6 +1,50 @@
 import { createElement } from '../render.js';
+import { humanizeDate } from '../utils.js';
+import { mockOffers } from '../mock/offers.js';
+import { mockDestinations } from '../mock/destinations.js';
+import { DATE_FORMAT_FULL } from '../const.js';
 
-function createNewPointTemplate() {
+function getEventTypeOffer(event) {
+  return mockOffers.find((offer) => offer.type === event.type);
+}
+
+function getEventDestination(event) {
+  return mockDestinations.find((destination) => destination.id === event.destination);
+}
+
+function createEventOffersTemplate(event) {
+  const eventTypeOffer = getEventTypeOffer(event);
+  const offersTemplate = eventTypeOffer
+    ? `
+      <div class="event__available-offers">
+        ${eventTypeOffer.offers.map((offer) => `
+          <div class="event__offer-selector">
+            <input
+              class="event__offer-checkbox  visually-hidden"
+              id="event-offer-${offer.title}-1"
+              type="checkbox"
+              name="event-offer-${offer.title}"
+              ${event.offers.includes(offer.id) ? 'checked' : ''}
+            >
+            <label class="event__offer-label" for="event-offer-${offer.title}-1">
+              <span class="event__offer-title">${offer.title}</span>
+              &plus;&euro;&nbsp;
+              <span class="event__offer-price">${offer.price}</span>
+            </label>
+          </div>
+        `).join('')}
+    ` : '';
+
+  return offersTemplate;
+}
+
+function createEditEventTemplate(event) {
+  const {type, basePrice, dateFrom, dateTo} = event;
+
+  const offersTemplate = createEventOffersTemplate(event);
+
+  const destination = getEventDestination(event);
+
   return (
     `<li class="trip-events__item">
       <form class="event event--edit" action="#" method="post">
@@ -8,7 +52,7 @@ function createNewPointTemplate() {
           <div class="event__type-wrapper">
             <label class="event__type  event__type-btn" for="event-type-toggle-1">
               <span class="visually-hidden">Choose event type</span>
-              <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+              <img class="event__type-icon" width="17" height="17" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
             </label>
             <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -66,22 +110,41 @@ function createNewPointTemplate() {
 
           <div class="event__field-group  event__field-group--destination">
             <label class="event__label  event__type-output" for="event-destination-1">
-              Flight
+              ${type}
             </label>
-            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
-            <datalist id="destination-list-1">
-              <option value="Amsterdam"></option>
-              <option value="Geneva"></option>
-              <option value="Chamonix"></option>
+            <input
+              class="event__input  event__input--destination"
+              id="event-destination-1"
+              type="text"
+              name="event-destination"
+              value="${destination.name}"
+              list="destination-list-2"
+            >
+            <datalist id="destination-list-2">
+              ${mockDestinations.map((mockDestination) => `
+                <option value="${mockDestination.name}"></option>
+              `).join(' ')}
             </datalist>
           </div>
 
           <div class="event__field-group  event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
+            <input
+              class="event__input  event__input--time"
+              id="event-start-time-1"
+              type="text"
+              name="event-start-time"
+              value="${humanizeDate(dateFrom, DATE_FORMAT_FULL)}"
+            >
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
-            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/19 00:00">
+            <input
+              class="event__input  event__input--time"
+              id="event-end-time-1"
+              type="text"
+              name="event-end-time"
+              value="${humanizeDate(dateTo, DATE_FORMAT_FULL)}"
+            >
           </div>
 
           <div class="event__field-group  event__field-group--price">
@@ -89,77 +152,30 @@ function createNewPointTemplate() {
               <span class="visually-hidden">Price</span>
               &euro;
             </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+            <input
+              class="event__input  event__input--price"
+              id="event-price-1"
+              type="text"
+              name="event-price"
+              value="${basePrice}"
+            >
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-          <button class="event__reset-btn" type="reset">Cancel</button>
+          <button class="event__reset-btn" type="reset">Delete</button>
+          <button class="event__rollup-btn" type="button">
+            <span class="visually-hidden">Open event</span>
+          </button>
         </header>
         <section class="event__details">
           <section class="event__section  event__section--offers">
             <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-            <div class="event__available-offers">
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-                <label class="event__offer-label" for="event-offer-luggage-1">
-                  <span class="event__offer-title">Add luggage</span>
-                  &plus;&euro;&nbsp;
-                  <span class="event__offer-price">30</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-                <label class="event__offer-label" for="event-offer-comfort-1">
-                  <span class="event__offer-title">Switch to comfort class</span>
-                  &plus;&euro;&nbsp;
-                  <span class="event__offer-price">100</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-                <label class="event__offer-label" for="event-offer-meal-1">
-                  <span class="event__offer-title">Add meal</span>
-                  &plus;&euro;&nbsp;
-                  <span class="event__offer-price">15</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-                <label class="event__offer-label" for="event-offer-seats-1">
-                  <span class="event__offer-title">Choose seats</span>
-                  &plus;&euro;&nbsp;
-                  <span class="event__offer-price">5</span>
-                </label>
-              </div>
-
-              <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-                <label class="event__offer-label" for="event-offer-train-1">
-                  <span class="event__offer-title">Travel by train</span>
-                  &plus;&euro;&nbsp;
-                  <span class="event__offer-price">40</span>
-                </label>
-              </div>
-            </div>
+            ${offersTemplate}
           </section>
 
           <section class="event__section  event__section--destination">
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-            <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
-
-            <div class="event__photos-container">
-              <div class="event__photos-tape">
-                <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-                <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-                <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-                <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-                <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
-              </div>
-            </div>
+            <p class="event__destination-description">${destination.description}</p>
           </section>
         </section>
       </form>
@@ -167,9 +183,13 @@ function createNewPointTemplate() {
   );
 }
 
-export default class NewPointView {
+export default class EditEventView {
+  constructor({event}) {
+    this.event = event;
+  }
+
   getTemplate() {
-    return createNewPointTemplate();
+    return createEditEventTemplate(this.event);
   }
 
   getElement() {
